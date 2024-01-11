9591fe97d7e2a8d5bde03444e231bb9c6e5ab2cf
Dan Chen <dan.chen@boardware.com>
2024-01-11 21:02:51 +0800

    feat

--- auth/auth.go
+++ auth/auth.go
@@ -1,6 +1,19 @@
 package auth
 
-import "github.com/universalmacro/common/utils"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/universalmacro/common/config"
+	"github.com/universalmacro/common/utils"
+)
+
+var secret []byte
+
+func init() {
+	secret = []byte(config.GetString("jwt.secret"))
+}
 
 type Password struct {
 	Password string `json:"password" gorm:"type:CHAR(128)"`
@@ -13,6 +26,29 @@ func (p *Password) SetPassword(password string) (string, []byte) {
 	p.Salt = salt
 	return hashed, salt
 }
+
 func (p *Password) PasswordMatching(password string) bool {
 	return utils.PasswordsMatch(p.Password, password, p.Salt)
+}
+
+func SignJwt(claims jwt.Claims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(secret)
+	return tokenString, err
+}
+
+func VerifyJwt(tokenString string) (jwt.MapClaims, error) {
+	token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return secret, nil
+	})
+	if token == nil {
+		return nil, errors.New("unvalid token")
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, errors.New("unvalid token")
 }
