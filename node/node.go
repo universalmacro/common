8620ead3b5c1f7faf01ee2a04089ce7025c7c7bf
Dan Chen <dan.chen@boardware.com>
2024-01-18 21:55:25 +0800

    feat

--- node/node.go
+++ node/node.go
@@ -1,6 +1,7 @@
 package node
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -8,7 +9,6 @@ import (
 
 	"github.com/universalmacro/common/config"
 	"github.com/universalmacro/common/singleton"
-	"github.com/universalmacro/core/controllers/models"
 )
 
 func newNodeConfigClient(apiUrl, nodeId, secretKey string) *NodeConfigClient {
@@ -25,23 +25,54 @@ type NodeConfigClient struct {
 	SecretKey string
 }
 
-func (n *NodeConfigClient) GetConfig() models.NodeConfig {
+type NodeConfig struct {
+	NodeID    uint
+	SecretKey string        `gorm:"type:varchar(64)"`
+	Api       *ApiConfig    `gorm:"type:json"`
+	Server    *ServerConfig `gorm:"type:json"`
+	Database  *DBConfig     `gorm:"type:json"`
+	Redis     *RedisConfig  `gorm:"type:json"`
+}
+type ApiConfig struct {
+	MerchantUrl string `json:"merchantUrl" gorm:"type:varchar(256)"`
+}
+type ServerConfig struct {
+	Port      string `json:"port" gorm:"type:varchar(64)"`
+	JwtSecret string `json:"jwtSecret" gorm:"type:varchar(64)"`
+}
+type DBConfig struct {
+	Host     string `json:"host" gorm:"type:varchar(64)"`
+	Port     string `json:"port" gorm:"type:varchar(64)"`
+	Username string `json:"username" gorm:"type:varchar(64)"`
+	Password string `json:"password" gorm:"type:varchar(64)"`
+	Database string `json:"database" gorm:"type:varchar(64)"`
+}
+type RedisConfig struct {
+	Host     string `json:"host" gorm:"type:varchar(64)"`
+	Port     string `json:"port" gorm:"type:varchar(64)"`
+	Password string `json:"password" gorm:"type:varchar(64)"`
+}
+
+func (n *NodeConfigClient) GetConfig() *NodeConfig {
 	c := http.Client{Timeout: time.Duration(1) * time.Second}
 	req, err := http.NewRequest("GET", n.ApiUrl+"/nodes/"+n.NodeId+"/config", nil)
 	req.Header.Add("ApiKey", n.SecretKey)
 	if err != nil {
 		fmt.Printf("error %s", err)
-		return models.NodeConfig{}
+		return nil
 	}
 	resp, err := c.Do(req)
 	if err != nil {
-		fmt.Printf("error %s", err)
-		return models.NodeConfig{}
+		return nil
 	}
 	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		panic("id or secret key is wrong")
+	}
 	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Println(body)
-	return models.NodeConfig{}
+	var config NodeConfig
+	json.Unmarshal(body, &config)
+	return &config
 }
 
 var nodeConfigClient = singleton.NewSingleton[NodeConfigClient](func() *NodeConfigClient {
