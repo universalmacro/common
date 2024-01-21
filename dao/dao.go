46f3dd840201f8856f24e776adaaceebdfcf09b2
Dan Chen <dan.chen@boardware.com>
2024-01-21 21:11:08 +0800

    feat

--- dao/dao.go
+++ dao/dao.go
@@ -7,30 +7,6 @@ import (
 	"gorm.io/gorm"
 )
 
-// var dbSingleton singleton.Singleton[gorm.DB] // singleton.NewSingleton[gorm.DB](CreateDBInstance, singleton.Lazy)
-
-// func GetDBInstance() *gorm.DB {
-// 	return dbSingleton.Get()
-// }
-
-// func Init(constructor func() *gorm.DB) {
-// 	dbSingleton = singleton.NewSingleton[gorm.DB](constructor, singleton.Lazy)
-// }
-
-// func CreateDBInstance() *gorm.DB {
-// 	db, err := NewConnection(
-// 		config.GetString("database.user"),
-// 		config.GetString("database.password"),
-// 		config.GetString("database.host"),
-// 		config.GetString("database.port"),
-// 		config.GetString("database.database"),
-// 	)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	return db
-// }
-
 func NewConnection(user, password, host, port, database string) (db *gorm.DB, err error) {
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
--- node/node_test.go
+++
@@ -1,8 +0,0 @@
-package node
-
-import "testing"
-
-func TestXxx(t *testing.T) {
-	client := newNodeConfigClient("https://uat.api.universalmacro.com/core", "1747545071349137408", "ABgiO0qO36O6FSsUlS9UxLVbviNopKoWhWaV9Ms8bfyqLNzlXvwIWPGDPUMZahtJ")
-	client.GetConfig()
-}
