ffbc5e3f2cf9f97b95d732a95190e7771c8ac474
Dan Chen <dan.chen@boardware.com>
2024-01-30 14:37:10 +0800

    feat

--- dao/data/phone.go
+++ dao/data/phone.go
@@ -1,6 +1,6 @@
 package data
 
 type PhoneNumber struct {
-	CountryCode string `json:"countryCode" gorm:"type:CHAR(6);index:,unique,composite:phone_number"`
-	Number      string `json:"number" gorm:"type:CHAR(11);index:,unique,composite:phone_number;column:phone_number"`
+	CountryCode string `json:"countryCode" gorm:"type:CHAR(6);index:unique,composite:phone_number"`
+	Number      string `json:"number" gorm:"type:CHAR(11);index:unique,composite:phone_number;column:phone_number"`
 }
