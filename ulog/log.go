c80dd3c9d663c4594fda04ac93b386c765c96b2d
Dan Chen <dan.chen@boardware.com>
2024-02-02 10:47:46 +0800

    feat

--- ulog/log.go
+++ ulog/log.go
@@ -20,18 +20,15 @@ func (h *Handler) Enabled(ctx context.Context, level slog.Level) bool {
 	return h.enabled <= level
 }
 
-func (h *Handler) Handle(ctx context.Context, e *slog.Record) error {
-	if e == nil {
-		return nil
-	}
-	fmt.Println(*e)
+func (h *Handler) Handle(ctx context.Context, e slog.Record) error {
+	fmt.Println(e)
 	return nil
 }
 
-func (h *Handler) WithAttrs(attrs []slog.Attr) Handler {
-	return *h
+func (h *Handler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return h
 }
 
-func (h *Handler) WithGroup(name string) Handler {
-	return *h
+func (h *Handler) WithGroup(name string) slog.Handler {
+	return h
 }
