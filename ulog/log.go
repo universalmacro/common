385bd1912cdd4283d4b3cd4b471a081201fc5ebf
Dan Chen <dan.chen@boardware.com>
2024-02-02 11:29:25 +0800

    feat

--- ulog/log.go
+++ ulog/log.go
@@ -14,6 +14,7 @@ func NewHandler(level slog.Level) *Handler {
 
 type Handler struct {
 	enabled slog.Level
+	attr    []slog.Attr
 }
 
 func (h *Handler) Enabled(ctx context.Context, level slog.Level) bool {
@@ -22,13 +23,17 @@ func (h *Handler) Enabled(ctx context.Context, level slog.Level) bool {
 
 func (h *Handler) Handle(ctx context.Context, e slog.Record) error {
 	fmt.Println(e.Message)
+	for _, attr := range h.attr {
+		fmt.Printf("%s=%v\n", attr.Key, attr.Value)
+	}
 	return nil
 }
 
-func (h *Handler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	return h
+func (h Handler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	h.attr = append(h.attr, attrs...)
+	return &h
 }
 
-func (h *Handler) WithGroup(name string) slog.Handler {
-	return h
+func (h Handler) WithGroup(name string) slog.Handler {
+	return &h
 }
